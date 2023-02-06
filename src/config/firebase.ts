59d8f9574a3367e468c80516f776b2d41391144b
ĐẶNG QUANG THỊNH <93868132+DangQuangThinh1999@users.noreply.github.com>
2023-02-06 23:05:42 +0700

    Feat/config firebase (#7)
    * feat:update
    
    * feat:update file firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,7 +8,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECTID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
